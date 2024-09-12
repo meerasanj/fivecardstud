4722b128f85f2e0e0ffe62ec94198b15b965a043
Meera P. Sanjeevirao <sanjeevirao_mp@cobra.cs.mercer.edu>
2024-09-12 19:56:22 -0400

    adding implementation file for Deck class

---
+++ cpp/deck.cpp
@@ -0,0 +1,48 @@
+#include "deck.h"
+#include <algorithm>
+#include <random>
+#include <iostream>
+
+Deck::Deck() {
+	for (int rank = 2; rank <= 14; rank++) {
+		cards.push_back(Card(rank, 'D'));
+		cards.push_back(Card(rank, 'C'));
+		cards.push_back(Card(rank, 'H'));
+		cards.push_back(Card(rank, 'S'));
+	}
+}
+
+void Deck::shuffle() {
+	// note: // i referenced https://stackoverflow.com/questions/5008804/how-do-you-generate-uniformly-distributed-random-integers/19728404#19728404
+	
+
+	std::random_device rd; // used to initialize engine
+	std::mt19937 rng(rd()); // seed generator for random number	
+	std::uniform_int_distribution<int> uni(0, cards.size() - 1); // to specify size
+
+	for (int i = 0; i < cards.size(); i++) {
+		int randomIndex = uni(rng);
+		std::swap(cards[i], cards[randomIndex]);
+	}
+}
+
+void Deck::dealHands(std::vector<std::vector<Card>>& hands) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 6; j++) {
+			hands[j].push_back(cards[0]);
+			cards.erase(cards.begin());
+		}
+	}
+}
+
+void Deck::printDeck(bool singleLine) const {
+	int count = 0;
+	for (int i = 0; i < cards.size(); i++) {
+		std::cout << cards[i].toString() << " ";
+		count++;
+		if (!singleLine && count % 13 == 0) {
+			std::cout << std::endl;
+		}
+	}
+	std::cout << std::endl;
+}
